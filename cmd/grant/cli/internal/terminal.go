e1ec2fc4001827aa8b14ccb063fc317e96ea8bfe
Will Murphy <willmurphyscode@users.noreply.github.com>
2026-02-20 08:47:45 -0500

    lint fix
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- cmd/grant/cli/internal/terminal.go
+++ cmd/grant/cli/internal/terminal.go
@@ -8,15 +8,15 @@ import (
 
 // IsTerminalOutput returns true if stdout is a terminal (not piped or redirected)
 func IsTerminalOutput() bool {
-	return term.IsTerminal(int(os.Stdout.Fd()))
+	return term.IsTerminal(int(os.Stdout.Fd())) // #nosec G115 -- file descriptors are safe to convert to int
 }
 
 // IsTerminalError returns true if stderr is a terminal (not piped or redirected)
 func IsTerminalError() bool {
-	return term.IsTerminal(int(os.Stderr.Fd()))
+	return term.IsTerminal(int(os.Stderr.Fd())) // #nosec G115 -- file descriptors are safe to convert to int
 }
 
 // IsTerminalInput returns true if stdin is a terminal (not piped or redirected)
 func IsTerminalInput() bool {
-	return term.IsTerminal(int(os.Stdin.Fd()))
+	return term.IsTerminal(int(os.Stdin.Fd())) // #nosec G115 -- file descriptors are safe to convert to int
 }
